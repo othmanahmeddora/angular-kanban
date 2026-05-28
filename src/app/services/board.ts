943710da1857c9cb93b5fc9cdd23abdd941cfbea
Othman Ahmed <othmanahmed258@gmail.com>
2026-05-28 10:20:51 +0300

    create: getBoards and getBoardByIndex functions

--- src/app/services/board.ts
+++ src/app/services/board.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@angular/core';
+import data from '../../../data.json';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Board {}
+export class Board {
+  private boards = data.boards;
+
+  getBoards() {
+    return this.boards;
+  }
+
+  getBoardByIndex(index: number) {
+    return this.boards[index];
+  }
+}
