4926c22b77e0a262b1415851b7e74ddbc294a5bc
Othman Ahmed <othmanahmed258@gmail.com>
2026-05-28 10:21:52 +0300

    create: boards dynamic route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AppMain } from './app-main/app-main';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'board/:index', component: AppMain },
+  { path: '', redirectTo: 'board/0', pathMatch: 'full' },
+];
